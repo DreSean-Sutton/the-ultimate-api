73cbeebc880c6c8af01955fade2eda6c6684677f
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-13 02:36:50 -0700

    Adding new databases to GitHub

---
+++ database/fighters-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.fighters ("fighter", "rosterId", "displayName")
+  VALUES
+    ('banjo', 73, 'banjo & kazooie'),
+    ('bayonetta', 63, 'bayonetta'),
+    ('bowser', 14, 'bowser'),
+    ('bowserJr', 58, 'bowser jr'),
+    ('byleth', 75, 'byleth'),
+    ('inkling', 64, 'inkling'),
+    ('joker', 71, 'joker');
