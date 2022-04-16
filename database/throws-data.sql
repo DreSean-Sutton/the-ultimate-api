918177a04c17ac8774326f11ae62b673dc3a0118
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-16 16:08:40 -0700

    Added public.throws table and its fighter data

---
+++ database/throws-data.sql
@@ -0,0 +1,143 @@
+INSERT INTO public.throws AS "banjo"
+  ("fighterId", "name", "type")
+  VALUES
+    (1, "grab", "throw"),
+    (1, "dash grab", "throw"),
+    (1, "pivot grab", "throw"),
+    (1, "pummel", "throw"),
+    (1, "forward throw", "throw"),
+    (1, "backward throw", "throw"),
+    (1, "up throw", "throw"),
+    (1, "down throw", "throw");
+
+INSERT INTO public.throws AS "bayonetta"
+  ("fighterId", "name", "type")
+  VALUES
+    (2, "grab", "throw"),
+    (2, "dash grab", "throw"),
+    (2, "pivot grab", "throw"),
+    (2, "pummel", "throw"),
+    (2, "forward throw", "throw"),
+    (2, "backward throw", "throw"),
+    (2, "up throw", "throw"),
+    (2, "down throw", "throw");
+
+INSERT INTO public.throws AS "bowser"
+  ("fighterId", "name", "type")
+  VALUES
+    (3, "grab", "throw"),
+    (3, "dash grab", "throw"),
+    (3, "pivot grab", "throw"),
+    (3, "pummel", "throw"),
+    (3, "forward throw", "throw"),
+    (3, "backward throw", "throw"),
+    (3, "up throw", "throw"),
+    (3, "down throw", "throw");
+
+INSERT INTO public.throws AS "bowserJr"
+  ("fighterId", "name", "type")
+  VALUES
+    (4, "grab", "throw"),
+    (4, "dash grab", "throw"),
+    (4, "pivot grab", "throw"),
+    (4, "pummel", "throw"),
+    (4, "forward throw", "throw"),
+    (4, "backward throw", "throw"),
+    (4, "up throw", "throw"),
+    (4, "down throw", "throw");
+
+INSERT INTO public.throws AS "byleth"
+  ("fighterId", "name", "type")
+  VALUES
+    (5, "grab", "throw"),
+    (5, "dash grab", "throw"),
+    (5, "pivot grab", "throw"),
+    (5, "pummel", "throw"),
+    (5, "forward throw", "throw"),
+    (5, "backward throw", "throw"),
+    (5, "up throw", "throw"),
+    (5, "down throw", "throw");
+
+INSERT INTO public.throws AS "captainFalcon"
+  ("fighterId", "name", "type")
+  VALUES
+    (6, "grab", "throw"),
+    (6, "dash grab", "throw"),
+    (6, "pivot grab", "throw"),
+    (6, "pummel", "throw"),
+    (6, "forward throw", "throw"),
+    (6, "backward throw", "throw"),
+    (6, "up throw", "throw"),
+    (6, "down throw", "throw");
+
+INSERT INTO public.throws AS "chrom"
+  ("fighterId", "name", "type")
+  VALUES
+    (7, "grab", "throw"),
+    (7, "dash grab", "throw"),
+    (7, "pivot grab", "throw"),
+    (7, "pummel", "throw"),
+    (7, "forward throw", "throw"),
+    (7, "backward throw", "throw"),
+    (7, "up throw", "throw"),
+    (7, "down throw", "throw");
+
+INSERT INTO public.throws AS "cloud"
+  ("fighterId", "name", "type")
+  VALUES
+    (8, "grab", "throw"),
+    (8, "dash grab", "throw"),
+    (8, "pivot grab", "throw"),
+    (8, "pummel", "throw"),
+    (8, "forward throw", "throw"),
+    (8, "backward throw", "throw"),
+    (8, "up throw", "throw"),
+    (8, "down throw", "throw");
+
+INSERT INTO public.throws AS "corrin"
+  ("fighterId", "name", "type")
+  VALUES
+    (9, "grab", "throw"),
+    (9, "dash grab", "throw"),
+    (9, "pivot grab", "throw"),
+    (9, "pummel", "throw"),
+    (9, "forward throw", "throw"),
+    (9, "backward throw", "throw"),
+    (9, "up throw", "throw"),
+    (9, "down throw", "throw");
+
+INSERT INTO public.throws AS "daisy"
+  ("fighterId", "name", "type")
+  VALUES
+    (10, "grab", "throw"),
+    (10, "dash grab", "throw"),
+    (10, "pivot grab", "throw"),
+    (10, "pummel", "throw"),
+    (10, "forward throw", "throw"),
+    (10, "backward throw", "throw"),
+    (10, "up throw", "throw"),
+    (10, "down throw", "throw");
+
+INSERT INTO public.throws AS "inkling"
+  ("fighterId", "name", "type")
+  VALUES
+    (11, "grab", "throw"),
+    (11, "dash grab", "throw"),
+    (11, "pivot grab", "throw"),
+    (11, "pummel", "throw"),
+    (11, "forward throw", "throw"),
+    (11, "backward throw", "throw"),
+    (11, "up throw", "throw"),
+    (11, "down throw", "throw");
+
+INSERT INTO public.throws AS "joker"
+  ("fighterId", "name", "type")
+  VALUES
+    (12, "grab", "throw"),
+    (12, "dash grab", "throw"),
+    (12, "pivot grab", "throw"),
+    (12, "pummel", "throw"),
+    (12, "forward throw", "throw"),
+    (12, "backward throw", "throw"),
+    (12, "up throw", "throw"),
+    (12, "down throw", "throw");
