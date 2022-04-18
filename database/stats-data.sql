79f6dc65bb2ff5b7e3ac6c664bf399048ccf6ad0
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-18 12:53:51 -0700

    Populated stats-data.sql with the Fighters' information

---
+++ database/stats-data.sql
@@ -0,0 +1,237 @@
+INSERT INTO public.stats AS "banjo"
+  ("fighterId", "name", "type")
+  VALUES
+    (1, 'weight', 'stat'),
+    (1, 'gravity', 'stat'),
+    (1, 'walk speed', 'stat'),
+    (1, 'run speed', 'stat'),
+    (1, 'initial dash', 'stat'),
+    (1, 'air speed', 'stat'),
+    (1, 'total air acceleration', 'stat'),
+    (1, 'sh/fh/shff/fhff', 'stat'),
+    (1, 'fall speed/fast fall speed', 'stat'),
+    (1, 'out of shield, ', 'stat'),
+    (1, 'out of shield, ', 'stat'),
+    (1, 'out of shield, ', 'stat'),
+    (1, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "bayonetta"
+  ("fighterId", "name", "type")
+  VALUES
+    (2, 'weight', 'stat'),
+    (2, 'gravity', 'stat'),
+    (2, 'walk speed', 'stat'),
+    (2, 'run speed', 'stat'),
+    (2, 'initial dash', 'stat'),
+    (2, 'air speed', 'stat'),
+    (2, 'total air acceleration', 'stat'),
+    (2, 'sh/fh/shff/fhff', 'stat'),
+    (2, 'fall speed/fast fall speed', 'stat'),
+    (2, 'out of shield, ', 'stat'),
+    (2, 'out of shield, ', 'stat'),
+    (2, 'out of shield, ', 'stat'),
+    (2, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "bowser"
+  ("fighterId", "name", "type")
+  VALUES
+    (3, 'weight', 'stat'),
+    (3, 'gravity', 'stat'),
+    (3, 'walk speed', 'stat'),
+    (3, 'run speed', 'stat'),
+    (3, 'initial dash', 'stat'),
+    (3, 'air speed', 'stat'),
+    (3, 'total air acceleration', 'stat'),
+    (3, 'sh/fh/shff/fhff', 'stat'),
+    (3, 'fall speed/fast fall speed', 'stat'),
+    (3, 'out of shield, ', 'stat'),
+    (3, 'out of shield, ', 'stat'),
+    (3, 'out of shield, ', 'stat'),
+    (3, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "bowserJr"
+  ("fighterId", "name", "type")
+  VALUES
+    (4, 'weight', 'stat'),
+    (4, 'gravity', 'stat'),
+    (4, 'walk speed', 'stat'),
+    (4, 'run speed', 'stat'),
+    (4, 'initial dash', 'stat'),
+    (4, 'air speed', 'stat'),
+    (4, 'total air acceleration', 'stat'),
+    (4, 'sh/fh/shff/fhff', 'stat'),
+    (4, 'fall speed/fast fall speed', 'stat'),
+    (3, 'out of shield, ', 'stat'),
+    (3, 'out of shield, ', 'stat'),
+    (3, 'out of shield, ', 'stat'),
+    (3, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "byleth"
+  ("fighterId", "name", "type")
+  VALUES
+    (5, 'weight', 'stat'),
+    (5, 'gravity', 'stat'),
+    (5, 'walk speed', 'stat'),
+    (5, 'run speed', 'stat'),
+    (5, 'initial dash', 'stat'),
+    (5, 'air speed', 'stat'),
+    (5, 'total air acceleration', 'stat'),
+    (5, 'sh/fh/shff/fhff', 'stat'),
+    (5, 'fall speed/fast fall speed', 'stat'),
+    (5, 'out of shield, ', 'stat'),
+    (5, 'out of shield, ', 'stat'),
+    (5, 'out of shield, ', 'stat'),
+    (5, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "captainFalcon"
+  ("fighterId", "name", "type")
+  VALUES
+    (6, 'weight', 'stat'),
+    (6, 'gravity', 'stat'),
+    (6, 'walk speed', 'stat'),
+    (6, 'run speed', 'stat'),
+    (6, 'initial dash', 'stat'),
+    (6, 'air speed', 'stat'),
+    (6, 'total air acceleration', 'stat'),
+    (6, 'sh/fh/shff/fhff', 'stat'),
+    (6, 'fall speed/fast fall speed', 'stat'),
+    (6, 'out of shield, ', 'stat'),
+    (6, 'out of shield, ', 'stat'),
+    (6, 'out of shield, ', 'stat'),
+    (6, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "chrom"
+  ("fighterId", "name", "type")
+  VALUES
+    (7, 'weight', 'stat'),
+    (7, 'gravity', 'stat'),
+    (7, 'walk speed', 'stat'),
+    (7, 'run speed', 'stat'),
+    (7, 'initial dash', 'stat'),
+    (7, 'air speed', 'stat'),
+    (7, 'total air acceleration', 'stat'),
+    (7, 'sh/fh/shff/fhff', 'stat'),
+    (7, 'fall speed/fast fall speed', 'stat'),
+    (7, 'out of shield, ', 'stat'),
+    (7, 'out of shield, ', 'stat'),
+    (7, 'out of shield, ', 'stat'),
+    (7, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "cloud"
+  ("fighterId", "name", "type")
+  VALUES
+    (8, 'weight', 'stat'),
+    (8, 'gravity', 'stat'),
+    (8, 'walk speed', 'stat'),
+    (8, 'run speed', 'stat'),
+    (8, 'initial dash', 'stat'),
+    (8, 'air speed', 'stat'),
+    (8, 'total air acceleration', 'stat'),
+    (8, 'sh/fh/shff/fhff', 'stat'),
+    (8, 'fall speed/fast fall speed', 'stat'),
+    (8, 'out of shield, ', 'stat'),
+    (8, 'out of shield, ', 'stat'),
+    (8, 'out of shield, ', 'stat'),
+    (8, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "corrin"
+  ("fighterId", "name", "type")
+  VALUES
+    (9, 'weight', 'stat'),
+    (9, 'gravity', 'stat'),
+    (9, 'walk speed', 'stat'),
+    (9, 'run speed', 'stat'),
+    (9, 'initial dash', 'stat'),
+    (9, 'air speed', 'stat'),
+    (9, 'total air acceleration', 'stat'),
+    (9, 'sh/fh/shff/fhff', 'stat'),
+    (9, 'fall speed/fast fall speed', 'stat'),
+    (9, 'out of shield, ', 'stat'),
+    (9, 'out of shield, ', 'stat'),
+    (9, 'out of shield, ', 'stat'),
+    (9, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "daisy"
+  ("fighterId", "name", "type")
+  VALUES
+    (10, 'weight', 'stat'),
+    (10, 'gravity', 'stat'),
+    (10, 'walk speed', 'stat'),
+    (10, 'run speed', 'stat'),
+    (10, 'initial dash', 'stat'),
+    (10, 'air speed', 'stat'),
+    (10, 'total air acceleration', 'stat'),
+    (10, 'sh/fh/shff/fhff', 'stat'),
+    (10, 'fall speed/fast fall speed', 'stat'),
+    (10, 'out of shield, ', 'stat'),
+    (10, 'out of shield, ', 'stat'),
+    (10, 'out of shield, ', 'stat'),
+    (10, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "darkPit"
+  ("fighterId", "name", "type")
+  VALUES
+    (11, 'weight', 'stat'),
+    (11, 'gravity', 'stat'),
+    (11, 'walk speed', 'stat'),
+    (11, 'run speed', 'stat'),
+    (11, 'initial dash', 'stat'),
+    (11, 'air speed', 'stat'),
+    (11, 'total air acceleration', 'stat'),
+    (11, 'sh/fh/shff/fhff', 'stat'),
+    (11, 'fall speed/fast fall speed', 'stat'),
+    (11, 'out of shield, ', 'stat'),
+    (11, 'out of shield, ', 'stat'),
+    (11, 'out of shield, ', 'stat'),
+    (11, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "darkSamus"
+  ("fighterId", "name", "type")
+  VALUES
+    (12, 'weight', 'stat'),
+    (12, 'gravity', 'stat'),
+    (12, 'walk speed', 'stat'),
+    (12, 'run speed', 'stat'),
+    (12, 'initial dash', 'stat'),
+    (12, 'air speed', 'stat'),
+    (12, 'total air acceleration', 'stat'),
+    (12, 'sh/fh/shff/fhff', 'stat'),
+    (12, 'fall speed/fast fall speed', 'stat'),
+    (12, 'out of shield, ', 'stat'),
+    (12, 'out of shield, ', 'stat'),
+    (12, 'out of shield, ', 'stat'),
+    (12, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "inkling"
+  ("fighterId", "name", "type")
+  VALUES
+    (13, 'weight', 'stat'),
+    (13, 'gravity', 'stat'),
+    (13, 'walk speed', 'stat'),
+    (13, 'run speed', 'stat'),
+    (13, 'initial dash', 'stat'),
+    (13, 'air speed', 'stat'),
+    (13, 'total air acceleration', 'stat'),
+    (13, 'sh/fh/shff/fhff', 'stat'),
+    (13, 'fall speed/fast fall speed', 'stat'),
+    (13, 'out of shield, ', 'stat'),
+    (13, 'out of shield, ', 'stat'),
+    (13, 'out of shield, ', 'stat'),
+    (13, 'shield grab (post-shieldstun)', 'stat');
+
+INSERT INTO public.stats AS "joker"
+  ("fighterId", "name", "type")
+  VALUES
+    (14, 'weight', 'stat'),
+    (14, 'gravity', 'stat'),
+    (14, 'walk speed', 'stat'),
+    (14, 'run speed', 'stat'),
+    (14, 'initial dash', 'stat'),
+    (14, 'air speed', 'stat'),
+    (14, 'total air acceleration', 'stat'),
+    (14, 'sh/fh/shff/fhff', 'stat'),
+    (14, 'fall speed/fast fall speed', 'stat'),
+    (14, 'out of shield, ', 'stat'),
+    (14, 'out of shield, ', 'stat'),
+    (14, 'out of shield, ', 'stat'),
+    (14, 'shield grab (post-shieldstun)', 'stat');
