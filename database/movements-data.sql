6464f6b66e78f37eae5dc49b6f58856187bff910
DreSean-Sutton <dreseansutton@gmail.com>
2022-04-18 00:38:26 -0700

    Added movement type data to all Fighters in the database and updated scripts to account for the 2 new tables

---
+++ database/movements-data.sql
@@ -0,0 +1,182 @@
+INSERT INTO public.movements AS "banjo"
+  ("fighterId", "name", "type")
+  VALUES
+    (1, 'spot dodge', 'movements'),
+    (1, 'forward roll', 'movements'),
+    (1, 'backwards roll', 'movements'),
+    (1, 'neutral air dodge', 'movements'),
+    (1, 'air dodge, down', 'movements'),
+    (1, 'air dodge, side + down', 'movements'),
+    (1, 'air dodge, left/right', 'movements'),
+    (1, 'air dodge, side + up', 'movements'),
+    (1, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "bayonetta"
+  ("fighterId", "name", "type")
+  VALUES
+    (2, 'spot dodge', 'movements'),
+    (2, 'forward roll', 'movements'),
+    (2, 'backwards roll', 'movements'),
+    (2, 'neutral air dodge', 'movements'),
+    (2, 'air dodge, down', 'movements'),
+    (2, 'air dodge, side + down', 'movements'),
+    (2, 'air dodge, left/right', 'movements'),
+    (2, 'air dodge, side + up', 'movements'),
+    (2, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "bowser"
+  ("fighterId", "name", "type")
+  VALUES
+    (3, 'spot dodge', 'movements'),
+    (3, 'forward roll', 'movements'),
+    (3, 'backwards roll', 'movements'),
+    (3, 'neutral air dodge', 'movements'),
+    (3, 'air dodge, down', 'movements'),
+    (3, 'air dodge, side + down', 'movements'),
+    (3, 'air dodge, left/right', 'movements'),
+    (3, 'air dodge, side + up', 'movements'),
+    (3, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "bowserJr"
+  ("fighterId", "name", "type")
+  VALUES
+    (4, 'spot dodge', 'movements'),
+    (4, 'forward roll', 'movements'),
+    (4, 'backwards roll', 'movements'),
+    (4, 'neutral air dodge', 'movements'),
+    (4, 'air dodge, down', 'movements'),
+    (4, 'air dodge, side + down', 'movements'),
+    (4, 'air dodge, left/right', 'movements'),
+    (4, 'air dodge, side + up', 'movements'),
+    (4, 'air dodge up', 'movements'),
+    (4, 'up b, air dodge', 'movements');
+
+INSERT INTO public.movements AS "byleth"
+  ("fighterId", "name", "type")
+  VALUES
+    (5, 'spot dodge', 'movements'),
+    (5, 'forward roll', 'movements'),
+    (5, 'backwards roll', 'movements'),
+    (5, 'neutral air dodge', 'movements'),
+    (5, 'air dodge, down', 'movements'),
+    (5, 'air dodge, side + down', 'movements'),
+    (5, 'air dodge, left/right', 'movements'),
+    (5, 'air dodge, side + up', 'movements'),
+    (5, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "captainFalcon"
+  ("fighterId", "name", "type")
+  VALUES
+    (6, 'spot dodge', 'movements'),
+    (6, 'forward roll', 'movements'),
+    (6, 'backwards roll', 'movements'),
+    (6, 'neutral air dodge', 'movements'),
+    (6, 'air dodge, down', 'movements'),
+    (6, 'air dodge, side + down', 'movements'),
+    (6, 'air dodge, left/right', 'movements'),
+    (6, 'air dodge, side + up', 'movements'),
+    (6, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "chrom"
+  ("fighterId", "name", "type")
+  VALUES
+    (7, 'spot dodge', 'movements'),
+    (7, 'forward roll', 'movements'),
+    (7, 'backwards roll', 'movements'),
+    (7, 'neutral air dodge', 'movements'),
+    (7, 'air dodge, down', 'movements'),
+    (7, 'air dodge, side + down', 'movements'),
+    (7, 'air dodge, left/right', 'movements'),
+    (7, 'air dodge, side + up', 'movements'),
+    (7, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "cloud"
+  ("fighterId", "name", "type")
+  VALUES
+    (8, 'spot dodge', 'movements'),
+    (8, 'forward roll', 'movements'),
+    (8, 'backwards roll', 'movements'),
+    (8, 'neutral air dodge', 'movements'),
+    (8, 'air dodge, down', 'movements'),
+    (8, 'air dodge, side + down', 'movements'),
+    (8, 'air dodge, left/right', 'movements'),
+    (8, 'air dodge, side + up', 'movements'),
+    (8, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "corrin"
+  ("fighterId", "name", "type")
+  VALUES
+    (9, 'spot dodge', 'movements'),
+    (9, 'forward roll', 'movements'),
+    (9, 'backwards roll', 'movements'),
+    (9, 'neutral air dodge', 'movements'),
+    (9, 'air dodge, down', 'movements'),
+    (9, 'air dodge, side + down', 'movements'),
+    (9, 'air dodge, left/right', 'movements'),
+    (9, 'air dodge, side + up', 'movements'),
+    (9, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "daisy"
+  ("fighterId", "name", "type")
+  VALUES
+    (10, 'spot dodge', 'movements'),
+    (10, 'forward roll', 'movements'),
+    (10, 'backwards roll', 'movements'),
+    (10, 'neutral air dodge', 'movements'),
+    (10, 'air dodge, down', 'movements'),
+    (10, 'air dodge, side + down', 'movements'),
+    (10, 'air dodge, left/right', 'movements'),
+    (10, 'air dodge, side + up', 'movements'),
+    (10, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "darkPit"
+  ("fighterId", "name", "type")
+  VALUES
+    (11, 'spot dodge', 'movements'),
+    (11, 'forward roll', 'movements'),
+    (11, 'backwards roll', 'movements'),
+    (11, 'neutral air dodge', 'movements'),
+    (11, 'air dodge, down', 'movements'),
+    (11, 'air dodge, side + down', 'movements'),
+    (11, 'air dodge, left/right', 'movements'),
+    (11, 'air dodge, side + up', 'movements'),
+    (11, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "darkSamus"
+  ("fighterId", "name", "type")
+  VALUES
+    (12, 'spot dodge', 'movements'),
+    (12, 'forward roll', 'movements'),
+    (12, 'backwards roll', 'movements'),
+    (12, 'neutral air dodge', 'movements'),
+    (12, 'air dodge, down', 'movements'),
+    (12, 'air dodge, side + down', 'movements'),
+    (12, 'air dodge, left/right', 'movements'),
+    (12, 'air dodge, side + up', 'movements'),
+    (12, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "inkling"
+  ("fighterId", "name", "type")
+  VALUES
+    (13, 'spot dodge', 'movements'),
+    (13, 'forward roll', 'movements'),
+    (13, 'backwards roll', 'movements'),
+    (13, 'neutral air dodge', 'movements'),
+    (13, 'air dodge, down', 'movements'),
+    (13, 'air dodge, side + down', 'movements'),
+    (13, 'air dodge, left/right', 'movements'),
+    (13, 'air dodge, side + up', 'movements'),
+    (13, 'air dodge up', 'movements');
+
+INSERT INTO public.movements AS "joker"
+  ("fighterId", "name", "type")
+  VALUES
+    (14, 'spot dodge', 'movements'),
+    (14, 'forward roll', 'movements'),
+    (14, 'backwards roll', 'movements'),
+    (14, 'neutral air dodge', 'movements'),
+    (14, 'air dodge, down', 'movements'),
+    (14, 'air dodge, side + down', 'movements'),
+    (14, 'air dodge, left/right', 'movements'),
+    (14, 'air dodge, side + up', 'movements'),
+    (14, 'air dodge up', 'movements');
