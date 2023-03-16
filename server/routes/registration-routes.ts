8b8f4cdbf7222e6e712e8f3b2bbbb0e7a0c51cbf
DreSean-Sutton <dreseansutton@gmail.com>
2023-03-16 02:51:07 -0700

    Route added for adding users to the user.Users table and changed test to reflect this

--- server/routes/registration-routes.ts
+++ server/routes/registration-routes.ts
@@ -1,3 +1,5 @@
+const { User } = require('../model/user-database');
+import { Sequelize } from 'sequelize';
 import ClientError from '../utils/client-error';
 import { client } from '../conn';
 import { Req, Res, QueryString } from '../utils/types-routes';
@@ -6,26 +8,21 @@ var express = require('express');
 const registrationRoutes = express.Router();
 
 registrationRoutes.post('/sign-up', async (req: Req, res: Res, next: (param1: any) => any) => {
-  const schemaName = 'test_schema';
-  const tableName = 'test_table';
+  const email = 'test_email';
+  const username = 'test_username';
+  const password = 'test_password';
 
-  const createSchemaQuery = `CREATE SCHEMA IF NOT EXISTS ${schemaName}`;
-  const createTableQuery = `
-  CREATE TABLE IF NOT EXISTS ${schemaName}.${tableName} (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL
-  )
-`;
+  const user = await User.create({
+    email: email,
+    username: username,
+    password: password
+  })
+  res.status(201).json(user);
+})
 
-  client.query(createSchemaQuery)
-    .then(() => client.query(createTableQuery))
-    .then(() => {
-      res.status(201).json({ message: (`Schema "${schemaName}" and table "${schemaName}.${tableName}" created.`)});
-    })
-    .catch((err: any) => {
-      console.error(err);
-      res.status(404).json({ error: err })
-    });
+registrationRoutes.delete('/delete-account', async (req: Req, res: Res, next: (para1: any) => any) => {
+  const user = await User.destroy({
+    truncate: true
+  });
 })
 module.exports = registrationRoutes;
--- test/e2e-tests/registration-routes.test.ts
+++ test/e2e-tests/registration-routes.test.ts
@@ -6,16 +6,32 @@ chai.use(chaiHttp);
 
 describe("POST /api/registration/sign-up", () => {
 
-  describe.only("successful request", () => {
+  // This is used for resetting database
+  function deleteUser(done: any) {
+    chai.request('http://localhost:5000')
+      .delete('/api/registration/delete-account')
+      .query({})
+      .end((err, res) => {
+        if(err) return done(err);
+        res.should.have.status(204);
+        console.log(res.body);
+        done();
+      })
+  }
+
+  describe("successful request", () => {
 
     it("returns a 201 when user is created", done => {
+      deleteUser(done);
+      const userKeys = ['id', 'email', 'username', 'password', 'updatedAt', 'createdAt'];
+
       chai.request('http://localhost:5000')
         .post('/api/registration/sign-up')
         .query({})
         .end((err, res) => {
           if(err) return done(err);
           res.should.have.status(201);
-          res.body.should.haveOwnProperty('message');
+          res.body.should.have.all.key(userKeys);
           console.log(res.body);
           done(err);
         })
