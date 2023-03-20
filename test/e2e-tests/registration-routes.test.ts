7c4b432477da507a0b5556df41b2dc3ad156988f
DreSean-Sutton <dreseansutton@gmail.com>
2023-03-20 11:41:46 -0700

    Added 3 tests for if email, username, and/or password are null

--- test/e2e-tests/registration-routes.test.ts
+++ test/e2e-tests/registration-routes.test.ts
@@ -4,17 +4,13 @@ import chaiHttp from 'chai-http';
 chai.should();
 chai.use(chaiHttp);
 
-describe.only("POST /api/registration/sign-up", () => {
+describe("POST /api/registration/sign-up", () => {
 
-  const params = {
-    email: 'test_email',
-    username: 'test_username',
-    password: 'test_password'
-  }
-
+  const url = 'http://localhost:5000';
+  const path = '/api/registration/sign-up';
   // This is used for resetting database
   function deleteUser() {
-    chai.request('http://localhost:5000')
+    chai.request(url)
       .delete('/api/registration/delete-account')
       .query({})
       .end((err, res) => {
@@ -25,14 +21,18 @@ describe.only("POST /api/registration/sign-up", () => {
 
   describe("successful request", () => {
 
-
+    const params = {
+      email: 'test_email',
+      username: 'test_username',
+      password: 'test_password'
+    }
 
     it("returns a 201 when user is created", done => {
-      deleteUser();
+      // deleteUser();
       const userKeys = ['id', 'email', 'username', 'password', 'updatedAt', 'createdAt'];
 
-      chai.request('http://localhost:5000')
-        .post('/api/registration/sign-up')
+      chai.request(url)
+        .post(path)
         .set('content-type', 'application/json')
         .send(params)
         .end((err, res) => {
@@ -48,11 +48,19 @@ describe.only("POST /api/registration/sign-up", () => {
 
     })
   })
+
   describe("unsuccessful request", () => {
+
+    const params = {
+      email: 'test_email',
+      username: 'test_username',
+      password: 'test_password'
+    }
+
     it("returns a 400 request if email already exists", done => {
 
-      chai.request('http://localhost:5000')
-        .post('/api/registration/sign-up')
+      chai.request(url)
+        .post(path)
         .set('content-type', 'application/json')
         .send(params)
         .end((err, res) => {
@@ -64,6 +72,93 @@ describe.only("POST /api/registration/sign-up", () => {
           res.body.should.have.property('errors');
           res.body.should.have.property('fields');
           res.body.fields.should.have.property('email');
+          done();
+        })
+    })
+    it("returns a 400 request if username already exists", done => {
+
+      params.email = 'test_email2'; // Changing this value so username is checked for uniqueness
+      chai.request(url)
+        .post(path)
+        .set('content-type', 'application/json')
+        .send(params)
+        .end((err, res) => {
+          if (err) {
+            console.log(err);
+            params.email = 'test_email'; // reseting value
+            return done(err);
+          }
+          res.should.have.status(400);
+          res.body.should.have.property('errors');
+          res.body.should.have.property('fields');
+          res.body.fields.should.have.property('username');
+          params.email = 'test_email'; // reseting value
+          done();
+        })
+    })
+    it("returns a 400 request if email is null", done => {
+
+      chai.request(url)
+        .post(path)
+        .set('content-type', 'application/json')
+        .send({
+          email: null,
+          username: params.username,
+          password: params.password
+        })
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
+          res.should.have.status(400);
+          res.body.should.have.property('errors');
+          res.body.errors[0].path.should.equal('email');
+          done();
+        })
+    })
+    it("returns a 400 request if username is null", done => {
+
+      chai.request(url)
+        .post(path)
+        .set('content-type', 'application/json')
+        .send({
+          email: params.email,
+          username: null,
+          password: params.password
+        })
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
+          res.should.have.status(400);
+          res.body.should.have.property('errors');
+          res.body.errors[0].path.should.equal('username');
+          done();
+        })
+    })
+    it("returns a 400 request if password is null", done => {
+
+      chai.request(url)
+        .post(path)
+        .set('content-type', 'application/json')
+        .send({
+          email: params.email,
+          username: params.username,
+          password: null
+        })
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
+          res.should.have.status(400);
+          res.body.should.have.property('errors');
+          res.body.errors[0].path.should.equal('password');
           done();
         })
     })
