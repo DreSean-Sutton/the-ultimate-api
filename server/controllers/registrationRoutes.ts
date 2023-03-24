20f49dafd2b73a64bc07eb53924c00d6b5f2500a
DreSean-Sutton <dreseansutton@gmail.com>
2023-03-23 22:03:03 -0700

    All get routes now follow a MVC format

--- server/controllers/registrationRoutes.ts
+++ server/controllers/registrationRoutes.ts
@@ -1,6 +1,4 @@
-const { User } = require('../model/user-database');
 const registrationController = require('../controllers/registrationController');
-import { Req, Res, QueryString } from '../utils/types-routes';
 
 var express = require('express');
 const registrationRoutes = express.Router();
