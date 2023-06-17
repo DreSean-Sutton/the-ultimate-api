9d68a72502cc0bd7cb8c7688b81d4f5baa91ff27
DreSean-Sutton <dreseansutton@gmail.com>
2023-06-17 01:29:00 -0700

    Added tests for '/api/update/movements/:id' route

--- test/e2e-tests/updateRoutes.test.ts
+++ test/e2e-tests/updateRoutes.test.ts
@@ -255,7 +255,7 @@ describe.only("PUT /api/update/throws/:id", () => {
   const url = 'http://localhost:5000';
 
   describe("Successful requests", () => {
-    it("Returns a 200 status if a fighter's throws updated", done => {
+    it("Returns a 200 status if a fighter's throws are updated", done => {
       chai.request(url)
         .put('/api/update/throws/718')
         .set('authorization', `Bearer ${testToken}`)
@@ -273,7 +273,7 @@ describe.only("PUT /api/update/throws/:id", () => {
           done();
         })
     })
-    it("Returns a 200 status if a fighter's throws updated", done => {
+    it("Returns a 200 status if a fighter's throws are updated", done => {
       chai.request(url)
         .put('/api/update/throws/719')
         .set('authorization', `Bearer ${testToken}`)
@@ -294,7 +294,7 @@ describe.only("PUT /api/update/throws/:id", () => {
   })
 
   describe("Unsuccessful Requests", () => {
-    it("returns a 404 status if moveId isn't found", done => {
+    it("returns a 404 status if throwId isn't found", done => {
       chai.request(url)
         .put('/api/update/throws/90000')
         .set('authorization', `Bearer ${testToken}`)
@@ -323,6 +323,87 @@ describe.only("PUT /api/update/throws/:id", () => {
             console.log(err);
             return done(err);
           }
+          res.should.have.status(400);
+          res.body.should.have.property('error');
+          done();
+        })
+    })
+  })
+})
+
+describe.only("POST /api/update/movements/:id", () => {
+  const url = 'http://localhost:5000';
+
+  describe("Successful requests", () => {
+    it("Returns a 200 status if a fighter's movements are updated", done => {
+      chai.request(url)
+        .put('/api/update/movements/804')
+        .set('authorization', `Bearer ${testToken}`)
+        .set('username', 'test_username')
+        .set('content-type', 'application/json')
+        .send({ name: 'air dodge', activeFrames: '6-22', totalFrames: '50' })
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          res.should.have.status(200);
+          res.body.should.have.property('message');
+          res.body.should.have.property('affectedFighterId');
+          done();
+        })
+    })
+    it("Returns a 200 status if another fighter's movements are updated", done => {
+      chai.request(url)
+        .put('/api/update/movements/805')
+        .set('authorization', `Bearer ${testToken}`)
+        .set('username', 'test_username')
+        .set('content-type', 'application/json')
+        .send({ name: 'air dodge', activeFrames: '8-20', totalFrames: '70' })
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          res.should.have.status(200);
+          res.body.should.have.property('message');
+          res.body.should.have.property('affectedFighterId');
+          done();
+        })
+    })
+  })
+  describe("Unsuccessful requests", () => {
+    it("returns a 404 status if movementId isn't found", done => {
+      chai.request(url)
+        .put('/api/update/movements/90000')
+        .set('authorization', `Bearer ${testToken}`)
+        .set('username', 'test_username')
+        .set('content-type', 'application/json')
+        .send({ activeFrames: '10', totalFrames: '20', damage: '20.0%', name: 'UwU'})
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
+          res.should.have.status(404);
+          res.body.should.have.property('error');
+          done();
+        })
+    })
+    it("returns a 400 status if no values were changed", done => {
+      chai.request(url)
+        .put('/api/update/movements/718')
+        .set('authorization', `Bearer ${testToken}`)
+        .set('username', 'test_username')
+        .set('content-type', 'application/json')
+        .send({})
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
           res.should.have.status(400);
           res.body.should.have.property('error');
           done();
