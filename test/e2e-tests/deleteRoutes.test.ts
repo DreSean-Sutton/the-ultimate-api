3a507f0b6bb881adad208e3f555b75631edec075
DreSean-Sutton <dreseansutton@gmail.com>
2023-06-22 02:59:46 -0700

    Added several tests for '/api/delete/fighters/:id' route

---
+++ test/e2e-tests/deleteRoutes.test.ts
@@ -0,0 +1,75 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import chaiHttp from 'chai-http';
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+chai.should();
+chai.use(chaiHttp);
+
+const testPayload = {
+  userId: 123,
+  exp: Math.floor(Date.now() / 1000) + (60 * 30)
+};
+const testToken = jwt.sign(testPayload, process.env.TOKEN_SECRET);
+
+describe.only("DELETE /api/delete/fighters/:id", () => {
+  const url = 'http://localhost:5000';
+
+  describe("Successful Requests", () => {
+
+    it("returns a 204 request when a fighter is deleted", done => {
+      chai.request(url)
+        .delete('/api/delete/fighters/90')
+        .set('authorization', `Bearer ${testToken}`)
+        .set('username', 'test_username')
+        .set('content-type', 'application/json')
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
+          res.should.have.status(204);
+          done();
+        })
+    })
+  })
+
+  describe("Unsuccessful Requests", () => {
+
+    it("returns a 404 request when a fighter isn't found", done => {
+      chai.request(url)
+        .delete('/api/delete/fighters/90000')
+        .set('authorization', `Bearer ${testToken}`)
+        .set('username', 'test_username')
+        .set('content-type', 'application/json')
+        .end((err, res) => {
+          if(err) {
+            console.log(err);
+            return done(err);
+          }
+          console.log(res.body);
+          res.should.have.status(404);
+          res.body.should.have.property('error')
+          done();
+        })
+    })
+    it("returns an error if the id is not a number", done => {
+    chai.request(url)
+      .delete('/api/delete/fighters/not_a_number')
+      .set('authorization', `Bearer ${testToken}`)
+      .set('username', 'test_username')
+      .set('content-type', 'application/json')
+      .end((err, res) => {
+        if(err) {
+          console.log(err);
+          return done(err);
+        }
+        console.log(res.body);
+        res.should.have.status(400);
+        res.body.should.have.property('error');
+        done();
+      })
+    })
+  })
+})
