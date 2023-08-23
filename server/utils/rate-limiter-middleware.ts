8b8804ab3d211e61dc4e5f9a792c6e14c058cdc6
DreSean-Sutton <dreseansutton@gmail.com>
2023-08-23 02:01:07 -0700

    Useless change added and rate limiting is ready for production (hopefully)

--- server/utils/rate-limiter-middleware.ts
+++ server/utils/rate-limiter-middleware.ts
@@ -17,7 +17,7 @@ if(process.env.NODE_ENV === 'development') {
 
 const rateLimiter = new RateLimiterMemory(opts);
 
-async function rateLimiterMiddleware(req: any, res: any, next: any) {
+function rateLimiterMiddleware(req: any, res: any, next: any) {
   const { authorization } = req.headers;
   const remoteAddress = req.ip;
 
