e39a75be22f3ea1578e12ea625267c07a9d64f51
DreSean-Sutton <dreseansutton@gmail.com>
2023-09-24 04:05:43 -0700

    Incorporated nock and supertest into project for testing

--- test/e2e-tests/resetRoutes.test.ts
+++ test/e2e-tests/resetRoutes.test.ts
@@ -1,6 +1,9 @@
 import chai from "chai";
 import sinon from "sinon";
 import chaiHttp from "chai-http";
+import nock from "nock";
+const request = require("supertest");
+const resetControllers = require('../../server/controllers/resetController');
 require('dotenv/config');
 const jwt = require('jsonwebtoken');
 chai.should();
@@ -13,43 +16,55 @@ const testPayload = {
 const testToken = jwt.sign(testPayload, process.env.TOKEN_SECRET);
 
 describe.only("POST /api/reset/getResetToken", () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
   const url = 'http://localhost:5000';
   const path = '/api/reset/get-reset-token';
 
   describe("successful requests", () => {
-    it("should send a token to a user's email address", done => {
-      chai.request(url)
-        .post(path)
-        .set('content-type', 'application/json')
-        .send({ email: 'test_email@gmail.com' })
-        .end((err, res) => {
-          if(err) {
-            console.log('An error has occurred: ', err);
-            return done(err);
-          }
-          console.log(res.body);
-          res.should.have.status(200);
-          res.body.should.not.have.property('error');
-          done();
-        })
+    it("should send a token to a user's email address", async () => {
+      nock(url).post(path).reply(200, {
+          status: 200, body: { accepted: ['test_email@gmail.com'], rejected: [] }
+        });
+
+        const res = await request(url).post(path)
+          .set('Content-Type', 'application/json')
+          .send({
+            email: 'test_email@gmail.com'
+          })
+        res.should.have.status(200);
+        res.body.should.not.have.property('error');
+      // chai.request(url)
+      //   .post(path)
+      //   .set('Content-Type', 'application/json')
+      //   .send({ email: 'test_email@gmail.com' })
+      //   .end((err, res) => {
+      //     if(err) {
+      //       console.log('An error has occurred: ', err);
+      //       return done(err);
+      //     }
+      //     console.log(res.body);
+      //     res.should.have.status(200);
+      //     res.body.should.not.have.property('error');
+      //     done();
+      //   })
 
     });
   });
   describe("Unsuccessful Requests", () => {
-    it("Returns a 404 request if email doesn't exist", done => {
-      chai.request(url)
-        .post(path)
-        .set('content-type', 'application/json')
-        .send({ email: 'i_dont_exist@gmail.com' })
-        .end((err, res) => {
-          if(err) {
-            console.log(err);
-            return done(err);
-          }
-          res.should.have.status(404);
-          res.body.should.have.property('error');
-          done();
+    it("Returns a 404 request if email doesn't exist", async () => {
+      nock(url).post(path).reply(404, {
+        error: "Email doesn't exist"
+      });
+
+      const res = await request(url).post(path)
+        .set('Content-Type', 'application/json')
+        .send({
+          email: 'i_dont_exist@gmail.com'
         })
+      res.should.have.status(404);
+      res.body.should.have.property('error');
     });
   });
 });
